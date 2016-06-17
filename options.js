098942d94fd5ac827aa5e5bd921d599ab8c82566
Marcus Hunger <hunger@sipgate.de>
2016-06-17 14:23:42 +0200

    load settings in options-dialog

--- options.js
+++ options.js
@@ -1,6 +1,28 @@
 
 window.setTimeout(function () {
 
+    var init = function() {
+        var uriToSipid = function(uri) {
+            var ret = /sip:(.*)@/.exec(uri);
+
+            if (ret !== null && ret.length > 1) {
+                return ret[1];
+            } else {
+                return "";
+            }
+        };
+
+        chrome.storage.sync.get([ "uri", "password" ], function(config) {
+            document.getElementById("sipid").value = uriToSipid(config["uri"]);
+            document.getElementById("password").value = config["password"];
+            document.getElementById("password").focus();
+            document.getElementById("sipid").focus();
+        });
+
+    };
+
+    init();
+
     document.getElementById("saveSipCredentialsButton").onclick = function(e) {
 
         var sipid = document.getElementById("sipid").value;
