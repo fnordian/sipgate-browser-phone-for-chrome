b545e03a1e2b960f7b9c251cda220df644c9448e
Marcus Hunger <marcus.hunger@gmail.com>
2016-09-16 23:27:30 +0200

    remove obsolete test-code

--- background.js
+++ background.js
@@ -441,8 +441,6 @@ chrome.runtime.onMessage.addListener(
             console.log(sender.tab ?
             "from a content script:" + sender.tab.url :
                 "from the extension");
-            if (request.greeting == "hello")
-                sendResponse({farewell: "goodbye"});
         }
 
         return true;
